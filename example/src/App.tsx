b364924526f4d29c0ce2f07a4b5f899acee9cd51
Fernando Lopez <fernandojalope343@icloud.com>
2024-04-17 12:24:46 -0400

    install dependecies

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,25 @@
-import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'dine-async';
+import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "verity-quik";
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+	return (
+		<ThemeProvider>
+			<View style={styles.container}>
+				<Text>aa</Text>
+			</View>
+		</ThemeProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	box: {
+		width: 60,
+		height: 60,
+		marginVertical: 20,
+	},
 });
